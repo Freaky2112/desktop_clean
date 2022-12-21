f82dbe62c234a79d65751b755fcb0faf6001b044
freaky <freaky2112@hotmail.com>
2022-12-20 22:30:25 -0500

    Split Roles to Categories

---
+++ Roles_base/bash/files/upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## Upgarde Distro Scripts
+
+if [ -d /etc/pacman.d ]
+then
+	## RUn the Arch version Update command
+	sudo pacman -Syu
+fi
+
+
+if [ -d /etc/apt ]
+then
+	## RUn the Debian version Update command
+	sudo apt update && sudo apt dist-upgrade -y && sudo snap refresh
+fi
+
+if [ -d /etc/yum ]
+then
+	## RUn the Centos version Update command
+	sudo yum update -y
+fi
+
+
+
