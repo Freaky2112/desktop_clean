549f890d0cfbd57b8326224d7e394da93495d3f4
freaky <freaky2112@hotmail.com>
2022-12-20 22:22:39 -0500

    Updated Bash

--- roles/bash/files/upgrade.sh
+++ roles/bash/files/upgrade.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
+# Variables
+errorlog=~/updater_error.log
+
+# Fucntions check error code 
+checkexit() {
+		if [ $? -ne 0 ]
+		then
+		    echo "An error occurred, please check the $errorlog file."
+		else
+		    echo "Update run without any issue System is Fully Patch Have a Nice Day :)"
+		fi
+}
+
+
 ## Upgarde Distro Scripts
 
 if [ -d /etc/pacman.d ]
 then
 	## RUn the Arch version Update command
-	sudo pacman -Syu
+	sudo pacman -Syu 1>/dev/null 2>>$errorlog
+	checkexit
 fi
 
 
 if [ -d /etc/apt ]
 then
-	## RUn the Debian version Update command
-	sudo apt update && sudo apt dist-upgrade -y && sudo snap refresh
+	## RUn the Debian version Update Upgrade and Snapupgrade command
+	sudo apt update 1>/dev/null 2>>$errorlog
+	checkexit
+	
+	sudo apt dist-upgrade -y 1>/dev/null 2>>$errorlog
+	checkexit
+	
+	sudo snap refresh 1>/dev/null 2>>$errorlog
+	checkexit
 fi
 
 if [ -d /etc/yum ]
 then
 	## RUn the Centos version Update command
-	sudo yum update -y
+	sudo yum update -y 1>/dev/null 2>>$errorlog
+	checkexit
 fi
 
 
